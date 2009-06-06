4f0139cc4b2fb0fb927cc599f90f1209cc832300
Brian Ford <bford@engineyard.com>
2009-06-06 11:48:21 -0700

    Initial pages site.

--- language/versions/symbol_1.9.rb
+++
@@ -1,7 +0,0 @@
-describe "A Symbol literal" do
-  it "can be an empty string" do
-    c = :''
-    c.class.should == Symbol
-    c.inspect.should == ':""'
-  end
-end
--- library/enumerator/to_enum_spec.rb
+++
@@ -1,6 +0,0 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
-require File.dirname(__FILE__) + '/shared/enum_for'
-
-describe "#to_enum" do
-  it_behaves_like :enumerator_enum_for, :enum_for
-end
